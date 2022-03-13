c11afeda0dceaaf253b3cf9ea41f4af282f660e0
unknown <tmarinovi@pmfst.hr>
2022-03-13 23:06:57 +0100

    prvi commiot

--- vjezbe1/zadatak_6.py
+++ vjezbe1/zadatak_6.py
@@ -1,15 +1,32 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+
+
 x=int(input('unesite x: '))
 y=int(input('uneiste y: '))
-q,w=[int(t) for t in input('unesite koordinate sredista: ').split()]
+q,w=[int(x) for x in input('unesite koordinate sredista: ').split()]
 r=int(input('unesite radijus: '))
 
 def polozaj():
-  d = r**2 - ((q-x)**2 + (w-y)**2)
+  d =math.sqrt((x-q)**2+(y-w)**2)
 
-  if(d>0):
-    print("tocka se nalazi izvan kruznice")
-  elif(d==0):
+  if r>d:
+    print("tocka se nalazi unutar kruznice")
+  elif d==0:
     print("tocka se nalazi na kruznici")
-  else:
-    print("tocka se nalazi u kruzici")
+  elif r<d:
+    print("tocka se nalazi izvan kruzice")
 polozaj()
+
+
+def graf():
+  
+  an = np.linspace(0, 2 * np.pi, 100)
+  plt.plot(3 * np.cos(an), 3 * np.sin(an))
+  plt.axis('equal')
+
+  plt.plot(x,y,'s')
+  plt.show()
+
+graf()
