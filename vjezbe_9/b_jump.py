977a16701e3d6ab8afe42977cf2f6e47f29077c7
unknown <tmarinovi@pmfst.hr>
2022-06-19 00:06:05 +0200

    ispravljeno

--- vjezbe_9/b_jump.py
+++ vjezbe_9/b_jump.py
@@ -1,90 +1,84 @@
-import math
-import numpy as np
-
 class BungeeJumping:
     def __init__(self):
-        self.x_lista = []
-        self.t_lista = []
-        self.K_lista = []
-        self.U_lista= []
-        self.Eel_lista= []
-        self.E_lista = []
-        
-    def init(self, mass, k, vo, A, h, l, otpor_zraka = True, rho = 1.225, Cd = 1, dt= 0.01, g=9.81):
-        self.mass = mass
+        self.x_list = []
+        self.t_list = []
+        self.K_list = []
+        self.U_list = []
+        self.Eel_list = []
+        self.E_list = []
+    
+    def init(self,m,k,v0,h,l,oz = True,ro = 1.22,cd = 1,A = 1,dt = 0.01):
+        self.m = m
         self.k = k
-        self.vo = vo
-        self.A = A
+        self.v = v0
         self.h = h
-        self.g = g
         self.x = h
         self.l = l
-        self.otpor_zraka = otpor_zraka
-        self.rho = rho
-        self.Cd = Cd
+        self.oz = oz
+        self.ro = ro
+        self.cd = cd
+        self.A = A
         self.dt = dt
         self.t = 0
-        self.x_lista.append(self.x)
-        self.t_lista.append(self.t)
-        self.K_lista.append(0)
-        self.U_lista.append(mass*g*h)
-        self.Eel_lista.append(0)
-        self.E_lista.append(mass*g*h)      
-        
+        self.x_list.append(self.x)
+        self.t_list.append(self.t)
+        self.K_list.append(0)
+        self.U_list.append(m*9.81*h)
+        self.Eel_list.append(0)
+        self.E_list.append(m*9.81*h)
+
     def reset(self):
-        self.mass = 0
+        self.m = 0
         self.k = 0
-        self.vo = 0
-        self.A = 0
-        self.h = 0
+        self.v = 0        
         self.x = 0
-        self.l = 0
+        self.t = 0
+        self.cd = 0
+        self.A = 0
         self.dt = 0
-        self.x_lista = []
-        self.t_lista = []
-        self.K_lista = []
-        self.U_lista= []
-        self.Eel_lista = []
-        self.E_lista = []
-        
-    def _acceleration_(self):
+        self.x_list = []
+        self.t_list = []
+        self.K_list = []
+        self.U_list = []
+        self.Eel_list = []
+        self.E_list = []
+
+    def __acc(self):
         dx = self.h - self.l - self.x
         if dx > 0:
-            a_1 = (self.k/self.mass)*dx
+            a_el = (self.k/self.m)*dx
+        else: 
+            a_el = 0
+        if self.oz:
+            a_oz = -(abs(self.v)*self.v*self.ro*self.cd*self.A)/(2*self.m)
         else:
-            a_1 = 0
-            
-        if self.otpor_zraka:
-            a_2 = -(abs(self.vo)*self.vo*self.rho*self.Cd*self.A)/(2*self.mass)
-        else:
-            a_2 = 0
-        
-        a = -self.g + a_1 + a_2
+            a_oz = 0
+        a = -9.81 + a_el + a_oz
         return a
-    
-    def _energy_(self):
+
+    def __energy(self):
         dx = self.h - self.l - self.x
         if dx > 0:
-            self.eel = (1/2)*self.k*dx*dx
+            self.Eel = (1/2)*self.k*dx*dx
         else:
-            self.eel = 0
-            
-        self.U = self.mass*self.g*self.x
-        self.K = (1/2)*self.mass*self.vo*self.vo
-        self.E = self.eel + self.U + self.K
-        
-    def _jumping_(self):
-        self.a = self._acceleration_()
-        self.vo += self.a * self.dt
-        self.x += self.vo * self.dt
+            self.Eel = 0
+        self.U = self.m*9.81*self.x
+        self.K = (1/2)*self.m*self.v*self.v
+        self.E = self.Eel + self.U + self.K
+
+    def __jump(self):
+        self.a = self.__acc()
+        self.v += self.a*self.dt
+        self.x += self.v*self.dt
         self.t += self.dt
-        self._energy_()
-        
-    def _jumping_t_(self,t):
-        self._jumping_()
-        self.x_lista.append(self.x)
-        self.t_lista.append(self.t)
-        self.Eel_lista.append(self.eel)
-        self.K_lista.append(self.K)
-        self.U_lista.append(self.U)
-        self.E_lista.append(self.E)
+        self.__energy()
+
+    def jump(self,t):
+        while self.t < t:
+            self.__jump()
+            self.x_list.append(self.x)
+            self.t_list.append(self.t)
+            self.Eel_list.append(self.Eel)
+            self.K_list.append(self.K)
+            self.U_list.append(self.U)
+            self.E_list.append(self.E)
--- vjezbe_9/zadatak_1.py
+++ vjezbe_9/zadatak_1.py
@@ -1,43 +1,47 @@
-import b_jump as bj
+import b_jump as bj 
 import matplotlib.pyplot as plt
 
-o_1 = bj.BungeeJumping()
+j1 = bj.BungeeJumping()
 
-o_1.init(100, 23, 0, 1, 70, 15)
-o_1._jumping_t_(30)
+j1.init(100,20,0,250,50,False)    #(m,k,v0,h,l,oz = True,ro = 1.22,cd = 1,A = 1,dt = 0.01)
+j1.jump(50)
 
-plt.figure('BJ',figsize=(15,10), dpi=70)
+plt.figure("Bungee jumping", figsize=(15, 10), dpi=80)
 fig = plt.subplot()
 
 plt.subplot(2,2,1)
-plt.plot(o_1.t_lista,o_1.x_lista)
+plt.plot(j1.t_list,j1.x_list)
 plt.xlabel("t [s]")
 plt.ylabel("x [m]")
-plt.title("bez otpora zraka")
+plt.title("Graf bez otpora zraka")
+#plt.show()
 
 plt.subplot(2,2,2)
-plt.plot(o_1.t_lista, o_1.E_lista, label = 'E_ukupno')
-plt.plot(o_1.t_lista, o_1.U_lista, label = 'E_potencijalna')
-plt.plot(o_1.t_lista, o_1.K_lista, label = 'E_kineticka')
-plt.plot(o_1.t_lista, o_1.Eel_lista, label = 'E_elasticna')
-plt.legend(loc = "upper right", prop={'size': 10})
-plt.show()
+plt.plot(j1.t_list, j1.E_list, label = "ukupna energija")
+plt.plot(j1.t_list, j1.K_list, label = "kineticka energija")
+plt.plot(j1.t_list, j1.U_list, label = "potencijalna energija")
+plt.plot(j1.t_list, j1.Eel_list, label = "elasticna energija")
+plt.title("Energija bez otpora zraka")
+plt.legend(loc = "upper right", prop={'size': 10} )
+#plt.show()
 
-o_1.reset()
- 
-o_1.init(100, 23, 0, 1, 70, 15)
-o_1._jumping_t_(30)
+j1.reset()
+
+j1.init(100,20,0,250,50)    #(m,k,v0,h,l,oz = True,ro = 1.22,cd = 1,A = 1,dt = 0.01)
+j1.jump(50)
 
 plt.subplot(2,2,3)
-plt.plot(o_1.t_lista,o_1.x_lista)
+plt.plot(j1.t_list,j1.x_list)
 plt.xlabel("t [s]")
 plt.ylabel("x [m]")
-plt.title("s otporom zraka")
+plt.title("Graf s otporom zraka")
+#plt.show()
 
 plt.subplot(2,2,4)
-plt.plot(o_1.t_lista, o_1.E_lista, label = 'E_ukupno')
-plt.plot(o_1.t_lista, o_1.U_lista, label = 'E_potencijalna')
-plt.plot(o_1.t_lista, o_1.K_lista, label = 'E_kineticka')
-plt.plot(o_1.t_lista, o_1.Eel_lista, label = 'E_elasticna')
+plt.plot(j1.t_list, j1.E_list, label = "ukupna energija")
+plt.plot(j1.t_list, j1.K_list, label = "kineticka energija")
+plt.plot(j1.t_list, j1.U_list, label = "potencijalna energija")
+plt.plot(j1.t_list, j1.Eel_list, label = "elasticna energija")
+plt.title("Energija s otporom zraka")
 plt.legend(loc = "upper right", prop={'size': 10} )
 plt.show()
